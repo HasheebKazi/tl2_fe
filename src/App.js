d9ef4b913a99f415fe6f0dcacae7aecd31f3f0d5
hasheebkazi <hasheeb.k@gmail.com>
2020-03-10 13:02:39 -0400

    frontend boilder plate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import classes from './App.module.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1 className={ classes.App } > Time Tracker </h1>
+                <ul> Time: Activity </ul>
+                <input type="text" name="time" />
+                <button> Add </button>
+            </div>
+        );
+    }
 }
-
 export default App;
